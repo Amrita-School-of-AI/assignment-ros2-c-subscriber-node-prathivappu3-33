3c21bc58d4ca5da8b821b67054243ab5c57107eb
prathivappu3-33 <prathivappu3@gmail.com>
2026-02-04 03:49:45 +0000

    Add ROS2 C++ subscriber node

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -1,51 +1,39 @@
-#include <chrono>
 #include <memory>
-#include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
-
-class PublisherNode : public rclcpp::Node
+class SubscriberNode : public rclcpp::Node
 {
 public:
-  PublisherNode() : Node("publisher_node"), counter_(0)
+  SubscriberNode() : Node("subscriber_node")
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("/counter", 10);
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "/chatter",
+      10,
+      std::bind(&SubscriberNode::topic_callback, this, std::placeholders::_1)
+    );
 
-    timer_ = this->create_wall_timer(
-      500ms,
-      std::bind(&PublisherNode::timer_callback, this));
-
-    RCLCPP_INFO(this->get_logger(), "Publisher node started");
+    RCLCPP_INFO(this->get_logger(), "Subscriber node started");
   }
 
 private:
-  void timer_callback()
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
-    std_msgs::msg::String message;
-    message.data = "Count: " + std::to_string(counter_);
-
-    publisher_->publish(message);
-
     RCLCPP_INFO(
       this->get_logger(),
-      "Publishing: '%s'",
-      message.data.c_str());
-
-    counter_++;
+      "I heard: '%s'",
+      msg->data.c_str()
+    );
   }
 
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  int counter_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<PublisherNode>());
+  rclcpp::spin(std::make_shared<SubscriberNode>());
   rclcpp::shutdown();
   return 0;
 }
